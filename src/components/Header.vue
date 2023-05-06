ca106c8dcab06df448dbc87c3bfe00e9b15093f8
Al-Amin Islam Hridoy <alaminislamhridoy@gmail.com>
2023-05-07 01:51:30 +0600

    responsiveness improved

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
     <header>
         <div class="head-div">
             <div class="shade">
-                <div class="container">
+                <div class="container py-3">
                     <div class="app-title">
                         <p><span>BUZZ</span> Love Calculator</p>
                     </div>
