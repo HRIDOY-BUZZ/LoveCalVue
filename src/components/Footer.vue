2983586f03c47be6a1f5bf1a3e2bbab2331b58e5
Al-Amin Islam Hridoy <36563890+HRIDOY-BUZZ@users.noreply.github.com>
2023-08-11 11:46:36 +0600

    footer updated

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,7 +5,7 @@
                 <div class="copyright-text">
                     <p>
                         <b>BUZZ LOVE CALCULATOR v3.2</b><br>
-                        © Copyright 2021 <a href="http://alaminislamhridoy.rf.gd/"><i>HRIDOY-BUZZ</i></a>. All rights reserved.
+                        © Copyright 2021 <a href="https://hridoybuzz.me/"><i>HRIDOY-BUZZ</i></a>. All rights reserved.
                     </p>
                 </div>
             </div>
